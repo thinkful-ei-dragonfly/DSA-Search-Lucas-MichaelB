d720b075d8c93dfb71f0794ab38b8427f58abbd8
michaelbravo777 <michael.bravo777@gmail.com>
2019-07-31 14:38:29 -0400

    excersice 2

--- src/App.js
+++ src/App.js
@@ -19,44 +19,40 @@ export default class App extends React.Component {
 
    return -1;
   }
-  binarySearch = (inputArray, inputValue) => {
-    const sortedArray = inputArray.sort()
-    const counter = 0
-    function binarySearch(array, value, start=0, end=array.length-1) {
-        counter++
-        if (start > end) return -1;
-        //find the midpoint and the item at the midpoint
-        let index = Math.floor((start + end) / 2);
-        let item = array[index];
+  binarySearch = (inputArray, inputValue, start=0, end=inputArray.length-1, counter=0) => {
+    counter++
+    console.log(counter)
+    if (start > end) return -1;
+    //find the midpoint and the item at the midpoint
+    let index = Math.floor((start + end) / 2);
+    let item = inputArray[index];
 
-        //if the middle element is the target them return that location
-        if (item === value) {
-            this.setState({
-              binaryResult: counter
-            })
-            return index;
-        }
-        //if the middle element is less than the target then the target lies
-        //on the right side so eliminate all left side and only
-        //consider after the middle to the end of the array
-        else if (item < value) {
-            return binarySearch(array, value, index + 1, end);
-        }
-        //if the middle element is greater than the target then the
-        //target is on the left side so the left of the middle
-        else if (item > value) {
-            return binarySearch(array, value, start, index - 1);
-        }
-    };
-    binarySearch(sortedArray, inputValue)
-    debugger;
+    //if the middle element is the target them return that location
+    if (item === inputValue) {
+        this.setState({
+          binaryResult: counter
+        })
+        return index;
+    }
+    //if the middle element is less than the target then the target lies
+    //on the right side so eliminate all left side and only
+    //consider after the middle to the end of the array
+    else if (item < inputValue) {
+        return this.binarySearch(inputArray, inputValue, index + 1, end, counter);
+    }
+    //if the middle element is greater than the target then the
+    //target is on the left side so the left of the middle
+    else if (item > inputValue) {
+        return this.binarySearch(inputArray, inputValue, start, index - 1, counter);
+    }
   }
   handleSubmit = (e) => {
     e.preventDefault()
     const inputValue = e.target.searchValue.value
     const inputArray = e.target.searchForm.value.split(' ')
     this.linearSearch(inputArray, inputValue)
-    this.binarySearch(inputArray, inputValue)
+    const sortedArray = inputArray.sort()
+    this.binarySearch(sortedArray, inputValue)
   }
   render() {
     return (
@@ -80,6 +76,17 @@ export default class App extends React.Component {
              <div>
                <h2>Linear Result</h2>
                <p>It took {this.state.linearResult} steps to find your answer</p>
+
+             </div>
+           )}
+        </div>
+        <div>
+          {this.state.binaryResult === null
+           ? ''
+           : (
+             <div>
+               <h2>Binary Result</h2>
+               <p>It took {this.state.binaryResult} steps to find your answer</p>
 
              </div>
            )}
